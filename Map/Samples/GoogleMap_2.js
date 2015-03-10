c19bbefd061a5f9b6eec1198bca8b5b8286eb46d
enqnzhd <nhk0527@empas.com>
2015-03-11 08:31:04 +0900

    2번째 파일 생성

---
+++ Map/Samples/GoogleMap_2.js
@@ -0,0 +1,305 @@
+﻿
+/* 
+    0.기본 지도 그리기
+    $self영역(div)에 구글맵 Div영역 및 커스텀 가능한 DIv영역 하나씩 추가해준다.
+    
+    1.커스텀영역 처리
+    
+    2.마커 추가
+*/
+;
+(function($) {
+    $.fn.GoogleMap_default = function(options) {
+
+        var $mapDiv; //구글맵 Div 영역
+        var $cusDiv; //커스텀 Div 영역
+        var map; //맵 객체
+
+        var tBaseOptions; //기본옵션 상속 객체
+        var tMapOptions; //지도 기본 옵션 상속 객체
+        var tMapStyleOptions; //지도 스타일 옵션 상속 객체
+        var tMarkerOptions; //마커 기본 옵션 상속 객체
+
+        var defaultPos = null; //초기 중심 좌표 객체
+
+        var arrPos = new Array(); //좌표값 배열
+        var arrSpotInfo = new Array(); ; //스팟관련 정보 배열
+        var arrMarks = new Array(); //마커배열
+
+        //설정가능 옵션들
+        var baseOptions = {
+            width: 300
+            , height: 200
+            , viewStyleMap: false //스타일맵
+            , viewMarkers: true //마커표시
+            , displayAllMarkers: true //마커전체 센터 정렬
+        };
+
+        //구글맵 설정 옵션
+        var mapOptions = {
+            zoom: 8
+            , center: new google.maps.LatLng(37.500, 127.036) //서울
+            , mapTypeControl: false
+            , scrollwheel: true //휠 줌 사용여부
+            , draggable: true //클릭 드래그
+            , panControl: false //위치이동 컨트롤
+            , scaleControl: false //지도 배율 컨트롤
+            , streetViewControl: false //스트리트뷰 컨트롤
+            , zoomControl: false //줌컨트롤
+            , mapTypeControlOptions: {
+                mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+            } //커스텀 맵일 경우 맵타입 설정
+        };
+
+            //구글맵 스타일 옵션
+            var mapStyleOptions =
+        [{
+            stylers: [
+                { hue: "#00ffe6" },
+                { saturation: -20 }
+              ]
+        }, {
+            featureType: "road",
+            elementType: "geometry",
+            stylers: [
+                { lightness: 100 },
+                { visibility: "simplified" }
+              ]
+        }, {
+            featureType: "road",
+            elementType: "labels",
+            stylers: [
+                { visibility: "off" }
+              ]
+        }];
+
+        var DefaultIcon = new google.maps.MarkerImage("images/iconSpot.png", null, null, null, new google.maps.Size(22, 26));
+
+        //마커 옵션
+        var markerOptions = {
+            draggeable: false
+        , animation: google.maps.Animation.DROP
+        , iconimageType: "default"
+        };
+
+        return this.each(function() {
+            var obj = $(this);
+
+            InitMapArea(obj); //초기화
+
+            drawMap(); //지도 그리기
+
+            if (tBaseOptions.viewStyleMap)
+                setStyleMap(); //스타일 적용
+
+            if (tBaseOptions.viewMarkers)
+                setMarkers(); //마커 그리기
+
+            if (tBaseOptions.displayAllMarkers)
+                setAllMarkerCenter(); //마커기준 영역 조절 및 zoom 조절
+
+        });
+
+        /* 0. 초기화 -------------------------------------------------------------*/
+
+        //초기화설정
+        function InitMapArea(obj) {
+            //패널 up up
+            createPanel(obj);
+            //맵 옵션 call
+            loadOptions();
+        }
+
+        /*패널에 컨트롤 올리기*/
+        function createPanel(obj) {
+            var $thisCon = obj;
+
+            if ($thisCon.html() != "")
+                $thisCon.html("");
+
+            $mapDiv = $("<div class='map-canvas'><div>");
+            $cusDiv = $("<div class='cus-canvas'><div>");
+
+            $thisCon.append($cusDiv);
+            $thisCon.append($mapDiv);
+        }
+
+        function loadOptions() {
+
+            initOptions();
+
+            if (options == null || options == undefined)
+                return; //문제있음 디폴트값으로 처리
+
+            if (options.length > 0) {
+                for (var i = 0; i < options.length; i++) {
+                    if (options[i]["type"] != undefined) {
+                        getOptionsInfo(options[i]["type"], options[i]["option"]);
+                    }
+                }
+            }
+        }
+
+        //기본 옵션들 상속 객체에 설정
+        function initOptions() {
+            tBaseOptions = baseOptions;
+            tMapOptions = mapOptions;
+            tMapStyleOptions = mapStyleOptions;
+            tMarkerOptions = markerOptions;
+        }
+
+        //배열에서 필요 내용만 추출
+        function getOptionsInfo(type, option) {
+
+            switch (type.toLowerCase()) {
+                case "defaultpos":
+                    //기본좌표
+                    defaultPos = new google.maps.LatLng(option["lat"], option["lng"]);
+                    tMapOptions = $.extend({}, tMapOptions, { center: defaultPos });
+                    break;
+                case "default":
+                    //기본 옵션
+                    tBaseOptions = $.extend({}, baseOptions, option);
+                    break;
+                case "map":
+                    //지도 옵션
+                    tMapOptions = $.extend({}, mapOptions, option);
+
+                    if (defaultPos != null)
+                        tMapOptions = $.extend({}, tMapOptions, { center: defaultPos });
+
+                    break;
+                case "mapstyle":
+                    //지도 스타일 옵션
+                    tMapStyleOptions = $.extend({}, mapStyleOptions, option);
+                    break;
+                case "spot":
+                    //스팟정보
+                    arrSpotInfo = new Array();
+
+                    if (option.length > 0) {
+                        for (var s = 0; s < option.length; s++) {
+
+                            var tmpPos = new google.maps.LatLng(option[s]["lat"], option[s]["lng"]);
+                            arrSpotInfo.push(Arraymerge(option[s], { "position": tmpPos }));
+                            //arrPos.push(tmpPos);
+                        }
+                    }
+
+                    break;
+                case "marker":
+                    //구글맵 마커 옵션
+                    tMarkerOptions = $.extend({}, markerOptions, option);
+                    break;
+                default:
+                    break;
+            }
+        }
+        /* 0. 초기화 -------------------------------------------------------------*/
+
+        /* 1. 지도그리기 ---------------------------------------------------------*/
+        function drawMap() {
+            $mapDiv.css({ width: tBaseOptions.width, height: tBaseOptions.height });
+
+            map = new google.maps.Map($mapDiv[0], tMapOptions);
+        };
+        /* 1. 지도그리기 ---------------------------------------------------------*/
+
+        /* 2. 지도 스타일 적용 ---------------------------------------------------*/
+        function setStyleMap() {
+            map.setOptions({
+                mapTypeControl: false
+            , mapTypeControlOptions: {
+                mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'cusMap']
+            }
+            });
+
+            map.mapTypes.set('cusMap', new google.maps.StyledMapType(tMapStyleOptions, { name: "cusMap" }));
+            map.setMapTypeId('cusMap');
+        };
+        /* 2. 지도 스타일 적용 ---------------------------------------------------*/
+
+        /* 2. 마커 스타일 적용 ---------------------------------------------------*/
+        function setMarkers() {
+            if (arrSpotInfo.length > 0) {
+
+                for (var i = 0; i < arrSpotInfo.length; i++) {
+                    var marker = new google.maps.Marker(tMarkerOptions);
+
+                    var icon = DefaultIcon;
+                    if (tMarkerOptions.iconimageType == "num") {
+                        icon = new google.maps.MarkerImage("images/iconSpot" + getNumIcon(i + 1) + ".png", null, null, null, new google.maps.Size(22, 26));
+                    }
+
+                    var markerOpt = {
+                        position: arrSpotInfo[i]["position"],
+                        icon: icon,
+                        title: arrSpotInfo[i]["title"],
+                        map: map
+                    };
+
+                    marker.setOptions(markerOpt);
+
+                    arrMarks.push(marker);
+
+                    if (i == (arrSpotInfo.length - 1)) {
+                        MoveTo(arrSpotInfo[i]["position"]);
+                    }
+                }
+            }
+        };
+        /* 2. 마커 스타일 적용 ---------------------------------------------------*/
+        
+        /* 공용 메서드 -----------------------------------------------------------*/
+
+        //두개배열 merge
+        function Arraymerge(ArrObj1, ArrObj2) {
+            for (var key in ArrObj2) {
+                if (ArrObj2.hasOwnProperty(key))
+                    ArrObj1[key] = ArrObj2[key]
+            }
+
+            return ArrObj1;
+        }
+
+        //지도이동
+        function MoveTo(pos) {
+            map.panTo(pos);
+        };
+
+        //모든 마커 뷰 및 센터 설정
+        function setAllMarkerCenter() {
+            if (arrMarks.length > 0) {
+                var bounds = new google.maps.LatLngBounds();
+                for (var i = 0; i < arrMarks.length; i++) {
+                    bounds.extend(arrMarks[i].getPosition());
+                }
+
+                map.setCenter(bounds.getCenter());
+
+                map.fitBounds(bounds);
+
+                map.setZoom(map.getZoom() - 1);
+
+                if (map.getZoom() > 13)
+                    map.setZoom(13);
+            }
+        }
+
+        //넘버 아이콘
+        function getNumIcon(index) {
+            //임의로 01이미지로 반환 고정
+            //            if (index < 10)
+            //                return '0' + index;
+            //            else
+            //                return index;
+
+
+            return '01';
+        }
+
+        /* 공용 메서드 -----------------------------------------------------------*/
+
+    }; //plugin function
+
+})(jQuery);
---
+++ Map/Samples/googleMap_2.aspx
@@ -0,0 +1,175 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="googleMap_2.aspx.cs" Inherits="Samples_DefaultMap_googleMap" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        body { margin : 0 0 0 20 ; font-size:small; }
+        .text { width : 30px; }
+       .canvas div.mapArea { float : left; width:500px; height:500px; border:1px solid Gray; margin: 10 10 0 0;}        
+    </style>
+    <script src="//code.jquery.com/jquery-1.10.2.js"></script>       
+    <script id="mapSC" src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&language=ko&libraries=weather"></script>     
+    <script src="GoogleMap_2.js"></script>    
+    <script type="text/javascript" >
+        function getContext(obj) {
+            var strVal = "";
+
+            strVal = "<div>title : " + obj["title"] + "<br/> lat : " + obj["lat"] + "<br/> lng : " + obj["lng"] + "</div>";
+
+            //            return "가나다라마바사아자차카타파하";
+            return strVal;
+        }
+        
+        var spotInfosCity = [
+//            { "title": "인천광역시", "lat": "37.4485517865239", "lng": "126.45111322403", "callBack": getContext, "가": "aa", "나": "bb", "다": "cc", "라": "dd" }
+             { "title": "런던", "lat": "51.508129", "lng": "-0.128005", "callBack": getContext }
+            , { "title": "브뤼셀", "lat": "50.8503396", "lng": "4.3517103", "callBack": getContext }
+            , { "title": "브뤼헤", "lat": "51.2094345", "lng": "3.2252377", "callBack": getContext }
+            , { "title": "암스테르담", "lat": "52.3702157", "lng": "4.8951679", "callBack": getContext }
+            , { "title": "뮌헨", "lat": "48.1448353", "lng": "11.5580067" }
+            , { "title": "만하임", "lat": "49.4846773", "lng": "8.476724" }
+            , { "title": "프라하", "lat": "50.0878114", "lng": "14.4204598" }
+            , { "title": "빈(비엔나)", "lat": "48.2081743", "lng": "16.3738189" }
+            , { "title": "베네치아(베니스)", "lat": "45.4343363", "lng": "12.3387844" }
+            , { "title": "밀라노", "lat": "45.463681", "lng": "9.1881714" }
+            , { "title": "피렌체", "lat": "43.7687324", "lng": "11.2569013" }
+            , { "title": "로마", "lat": "41.8905198", "lng": "12.4942486" }
+            , { "title": "인스부르크", "lat": "47.2692124", "lng": "11.4041024" }
+            , { "title": "취리히", "lat": "47.367347", "lng": "8.5500025" }
+            , { "title": "인터라켄", "lat": "46.6854831", "lng": "7.8559015" }
+            , { "title": "파리", "lat": "48.856614", "lng": "2.3522219" }
+        ];
+    </script>
+    <script type="text/javascript">
+
+        $(document).ready(function() {
+            $(".mapArea").GoogleMap_default();
+        });
+
+        function ApplyOptions(tar) {
+            var options = null;
+            Options = getOptions();
+
+            if (tar == "a")
+                $("#map_1").GoogleMap_default(Options);
+            else if (tar == "b")
+                $(".mapArea").eq(1).GoogleMap_default(Options);
+            else if (tar == "all")
+                $(".mapArea").GoogleMap_default(Options);
+            else
+                alert("?????");
+        }
+
+        function getOptions() {
+            var defaultOption = {};
+            var defaultPosOption = {};
+            var mapOption = {};
+            var spotOption = {};
+            var markerOption = {};
+            
+            defaultPosOption = { lat: $("#txtLat").val()
+                                , lng: $("#txtLng").val()
+            };            
+
+            defaultOption = { width: $("#txtWidth").val()
+                            , height: $("#txtHeight").val()
+                            , viewStyleMap: $("#chkViewStyleMap").is(':checked')
+                            , viewMarkers: $("#chkViewMarkers").is(':checked')
+                            , displayAllMarkers: $("#chkDisplayAllMarkers").is(':checked')
+            };
+
+            if ($("#chkMapEnable").is(':checked')) {
+
+                mapOption = {
+                    zoom: Number($("#ddlZoom").val())
+                    , center: new google.maps.LatLng($("#txtLat").val(), $("#txtLng").val()) //서울
+                    , mapTypeControl: $("#chkMapTypeControl").is(':checked')
+                    , scrollwheel: $("#chkScrollwheel").is(':checked') //휠 줌 사용여부
+                    , draggable: $("#chkDraggable").is(':checked') //클릭 드래그
+                    , panControl: $("#chkPanControl").is(':checked') //위치이동 컨트롤
+                    , scaleControl: $("#chkScaleControl").is(':checked') //지도 배율 컨트롤
+                    , streetViewControl: $("#chkStreetViewControl").is(':checked') //스트리트뷰 컨트롤
+                    , zoomControl: $("#chkZoomControl").is(':checked') //줌컨트롤                
+                };
+            }
+
+            if ($("#chkViewMarkers").is(':checked')) {
+                spotOption = spotInfosCity;
+
+                markerOption = {
+                    draggeable: $("#chkDraggeable").is(':checked')
+                    , animation: $("#chkMarkerAni").is(':checked') ? google.maps.Animation.DROP : ""
+                    , iconimageType: $("#chkMarkerNum").is(':checked') ? "num" : "default"                    
+                };
+            }
+
+            Options = [
+                { type: "default", option: defaultOption }
+                , { type: 'defaultPos', option: defaultPosOption }
+                , { type: "map", option: mapOption }
+                , { type: "spot", option: spotOption }
+                , { type: "marker", option: markerOption }
+            ];
+
+            return Options;
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h3>구글맵 Test- 지도 + 커스텀 영역 + 마커</h3>
+        [지도]<br/>
+        width :<input class="text" type="text" id="txtWidth" value="500" />
+        height :<input class="text" type="text" id="txtHeight" value="500" />
+        center :[ Lat :<input class="text" type="text" id="txtLat" value="36.646" />, Lng :<input class="text" type="text" id="txtLng" value="136.979" /> ]
+        <br/>
+        [맵기본]<br/>
+        enable :<input type="checkbox" id="chkMapEnable" checked="checked" />
+        zoom : <select id="ddlZoom">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8" selected="selected">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+        </select>        
+        mapTypeControl :<input type="checkbox" id="chkMapTypeControl" checked="checked" />
+        scrollwheel :<input type="checkbox" id="chkScrollwheel" checked="checked" />
+        draggable :<input type="checkbox" id="chkDraggable" checked="checked" />
+        panControl :<input type="checkbox" id="chkPanControl" checked="checked" />
+        scaleControl :<input type="checkbox" id="chkScaleControl" checked="checked" />
+        streetViewControl :<input type="checkbox" id="chkStreetViewControl" checked="checked" />
+        zoomControl :<input type="checkbox" id="chkZoomControl" checked="checked" />
+        <br/>
+        [스타일맵]<br/>
+        enable :<input type="checkbox" id="chkViewStyleMap" checked="checked" />
+        <br/>
+        [마커]<br/>
+        enable :<input type="checkbox" id="chkViewMarkers" checked="checked" />
+        <span style="color:Red;">draggeable :<input type="checkbox" id="chkDraggeable" disabled="disabled" /></span>
+        markerAni :<input type="checkbox" id="chkMarkerAni" checked="checked" />
+        IconType[ <input type="radio" id="chkMarkerDefault" checked="checked" name="markerIconType" />default,  <input type="radio" id="chkMarkerNum" name="markerIconType" />Num]
+        viewAllMarkers :<input type="checkbox" id="chkDisplayAllMarkers" checked="checked" />
+        <br/>
+        
+        
+        <input type="button" value="첫번째 적용" onclick="ApplyOptions('a');" />
+        <input type="button" value="두번째 적용" onclick="ApplyOptions('b');" />
+        <input type="button" value="둘다 적용" onclick="ApplyOptions('all');" />
+        
+        <div class="canvas">
+            <div id="map_1" class="mapArea" ></div>
+            <div id="map_2" class="mapArea" ></div>
+        </div>
+    </div>
+    </form>
+</body>
+</html>
